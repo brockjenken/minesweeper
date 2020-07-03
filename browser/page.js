bd04c6fd1be87fd8a0a43797e2dba369040ea05f
mmema <marin_mema@sfu.ca>
2020-07-03 11:12:16 -0700

    Big tiles

--- browser/page.js
+++ browser/page.js
@@ -5,7 +5,7 @@ const N_ROOMS = 8;
 const TMP_N = 24;
 const TMP_M = 24;
 const TMP_R = Math.floor((TMP_N + TMP_M) * 2);
-const TMP_CELLSIZE = 24;
+const TMP_CELLSIZE = 30;
 
 const board = new MinesweeperBoard(TMP_N, TMP_M, TMP_CELLSIZE, TILESHEET, report_click);
 
